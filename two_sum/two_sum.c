823380089df964d0b6757daffa9a38532ac0a081
davidpacecode <david.pace@gmail.com>
2024-11-22 22:33:48 -0600

    finished c version

--- two_sum/two_sum.c
+++ two_sum/two_sum.c
@@ -3,33 +3,34 @@
 
 int*
 twoSum(int*, int, int, int*);
-    
 
 int
 main (void) {
 
-  int nums[] = {-4,34,-19,2,7,11,15};
-  int target = 36;
+  int nums[] = {2,7,11,15};
+  int target = 9;
+  int returnSize;
   int *indices;
 
-  indices = twoSum(nums,4,target,indices);
+  indices = twoSum(nums,4,target,&returnSize);
   printf("%d, %d\n",indices[0],indices[1]);
 
   return 0;
-
 }
 
 int*
 twoSum(int* nums, int numsSize, int target, int* returnSize) {
   
-  int* ret_ptr = (int*)malloc(2*sizeof(int));
+  *returnSize = 2;
   int i=0, j=1;
+  int *ret_ptr = (int*)malloc(2*sizeof(int));
 
   for(i=0;i<numsSize-1;i++)
-    for(j=1;j<numsSize;j++)
+    for(j=i+1;j<numsSize;j++)
       if(nums[i]+nums[j] == target) {
         ret_ptr[0]=i;
         ret_ptr[1]=j;
         return ret_ptr;
       }
+  return ret_ptr; /* technically this is an error condition */
 }
