c957030aad1d9d59715055a30bb60852c5bbf953
davidpacecode <david.pace@gmail.com>
2024-11-30 21:33:49 -0600

    got the improved version of jump game working

--- jump_game/ruby/jump_game_improved.rb
+++ jump_game/ruby/jump_game_improved.rb
@@ -1,24 +1,23 @@
 # @param {Integer[]} nums
 # @return {Boolean}
+
 def can_jump(nums)
 
-  ret_val = true
-  nums.reverse_each do |num|
+  max_reach = nums[0]
 
-    if num == 0
-      ret_val = false
-      nums.index(num).downto(0) do |i|
-        if nums[i] > nums.index(num) - nums.index(i)
-          ret_val = true
-          break
-        end
-      end
+  nums.each_with_index do |num, index|
+    max_reach = index + num if index + num > max_reach
+#    puts "num is #{num} max_reach is #{max_reach} nums.length -1 is #{nums.length - 1} current index of num is #{index}"
+    return true if max_reach >= nums.length - 1
+
+    if num == 0 && max_reach <= index
+      return false
     end
   end
-  ret_val
 end
 
-
+# numbers = [2,3,1,1,4]
 # numbers = [3,2,1,0,4]
 numbers = [2,0,6,9,8,4,5,0,8,9,1,2,9,6,8,8,0,6,3,1,2,2,1,2,6,5,3,1,2,2,6,4,2,4,3,0,0,0,3,8,2,4,0,1,2,0,1,4,6,5,8,0,7,9,3,4,6,6,5,8,9,3,4,3,7,0,4,9,0,9,8,4,3,0,7,7,1,9,1,9,4,9,0,1,9,5,7,7,1,5,8,2,8,2,6,8,2,2,7,5,1,7,9,6]
+
 puts "Jumping through #{numbers.to_s}....#{can_jump(numbers).to_s}"
---
+++ jump_game/ruby/leet_code_results.md
@@ -0,0 +1,8 @@
+Timed out for initial version...though I believe it worked.
+
+Improved version:
+
+**Runtime** 19ms Beats 62.50%
+
+**Memory** 216.58 MB Beats 12.11%
+
