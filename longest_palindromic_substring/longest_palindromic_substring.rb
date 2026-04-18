d037d2dd626b811c293c930b7d9645075730a675
davidpacecode <davidpace@gmail.com>
2026-04-17 22:42:43 -0500

    working on longest_palindromic_substring

--- longest_palindromic_substring/longest_palindromic_substring.rb
+++ longest_palindromic_substring/longest_palindromic_substring.rb
@@ -2,11 +2,39 @@
 
 def longest_palindrome(s)
 
-  longest = 0
+  return s if s == s.reverse
 
-  index = s.length - 1
-  index.downto(0) do |i|
-    longest = i if s[0..index] == s[index..0] &&
+  longest = 0
+  index = 0
+
+  until index == (s.length) do
+    # puts s[index..s.length - 1]
+    if s[index..s.length - 1] == s[index..s.length - 1].reverse
+      longest = s[index..s.length - 1].length
+      longest_string = s[index..s.length - 1]
+      break
+    end
+    index += 1
   end
+
+  index = s.length - 2
+
+  until index < 0 do
+    # puts s[0..index]
+    if s[0..index] == s[0..index].reverse
+      return s[0..index] if s[0..index].length > longest
+    end
+    index -= 1
+  end
+
+  longest_string
 end
 
+lp = longest_palindrome("abba")
+puts lp.to_s
+puts
+puts
+puts
+
+lp = longest_palindrome("daskfafhdgkhsflksjalfs")
+puts lp.to_s
